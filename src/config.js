5b47f04254fdc222a27d84fa80bbd41888f98e0f
Zayar Khin <arandomname@gmail.com>
2019-12-19 22:29:03 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://zayar@localhost/visiri',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://zayar@localhost/visiri-test',
-  JWT_SECRET: process.env.JWT_SECRET || 'xjixx76871i2h3uix898u12312jhgjkx',
+  JWT_SECRET: process.env.JWT_SECRET || 'token-changed-in-production',
 }
